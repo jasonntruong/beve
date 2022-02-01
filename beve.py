9f8ef08f76fd6bb2a831cadff2ce791793b4789d
jason <jason.truong@ryerson.ca>
2022-02-01 14:29:49 -0500

    starts up

--- beve.py
+++ beve.py
@@ -10,3 +10,22 @@ from os import system
 from dotenv import load_dotenv
 
 load_dotenv()
+
+tokenFile = open("token.txt", "r")
+TOKEN = tokenFile.read()
+
+client = commands.Bot(command_prefix = 'beve.')
+
+@client.event
+async def on_ready():
+    print('Beve is logged in')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="RnBeve"))
+
+@client.event
+async def on_message(message):
+    # await client.process_commands(message)
+
+    if message.content == 'beve.hi':
+        await message.channel.send('hi')
+
+client.run(TOKEN)
